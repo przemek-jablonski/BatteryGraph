05e998eddae88ae5bae83b2249f2813962b28dc3
ciemek <sharaquss@gmail.com>
2017-11-04 16:18:50 +0100

    batteryStatusReceiver turned off in Activity, new Foreground Service, monitoring there

--- app/src/main/java/com/android/szparag/batterygraph/utils/Extensions.kt
+++ app/src/main/java/com/android/szparag/batterygraph/utils/Extensions.kt
@@ -1,6 +1,6 @@
 package com.android.szparag.batterygraph.utils
 
-import android.app.Activity
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -14,27 +14,26 @@ import android.os.BatteryManager.EXTRA_TEMPERATURE
 import android.os.BatteryManager.EXTRA_VOLTAGE
 import android.os.Bundle
 import com.android.szparag.batterygraph.events.BatteryStatusEvent
+import timber.log.Timber
 
 /**
  * Created by Przemyslaw Jablonski (github.com/sharaquss, pszemek.me) on 01/11/2017.
  */
 
-fun Activity.createRegisteredBroadcastReceiver(
+fun Context.createRegisteredBroadcastReceiver(
     vararg intentFilterActions: String, callback: (Intent) -> (Unit)): BroadcastReceiver {
+  Timber.d("createRegisteredBroadcastReceiver, intentFilterActions: $intentFilterActions, callback: $callback, context: $this")
   val broadcastReceiver = object : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
       intent?.takeIf { intentFilterActions.contains(it.action) }?.let(callback::invoke)
     }
   }
-  this.registerReceiver(
-      broadcastReceiver,
-      IntentFilter().apply { intentFilterActions.forEach(this::addAction) }
-  )
+  this.registerReceiver(broadcastReceiver, IntentFilter().apply { intentFilterActions.forEach(this::addAction) })
   return broadcastReceiver
 }
 
-fun BroadcastReceiver.unregisterReceiverFromActivity(activity: Activity) {
-  activity.unregisterReceiver(this)
+fun BroadcastReceiver.unregisterReceiverFromContext(context: Context) {
+  context.unregisterReceiver(this)
 }
 
 fun Bundle.mapToBatteryStatusEvent() = BatteryStatusEvent(
@@ -49,3 +48,22 @@ fun Bundle.mapToBatteryStatusEvent() = BatteryStatusEvent(
 fun invalidIntValue() = -1
 fun invalidLongValue() = -1L
 fun invalidFloatValue() = -1f
+
+fun Intent.toPendingIntent(context: Context, requestCode: Int = 0, flags: Int = 0) =
+    PendingIntent.getActivity(context, requestCode, this, flags)
+
+//todo refactor that, this may be cool, but code quality needs to be better
+fun Intent.asString() = StringBuilder(1024).append(
+    "Intent: [action: ${this.action}, cat: ${this.categories}, component: ${this.component}, flags: ${this.flags} bundle: ${this
+        .extras.asString(StringBuilder(1024))}]")
+
+fun Bundle?.asString(stringBuilder: StringBuilder): StringBuilder {
+  if (this == null) return stringBuilder.append("[null]")
+  val keys = this.keySet()
+  stringBuilder.append("[")
+  keys.forEach { key ->
+    stringBuilder.append("$key: ${this.get(key)}, ")
+  }
+  stringBuilder.delete(stringBuilder.length - 2, stringBuilder.length - 1).append("]")
+  return stringBuilder
+}
