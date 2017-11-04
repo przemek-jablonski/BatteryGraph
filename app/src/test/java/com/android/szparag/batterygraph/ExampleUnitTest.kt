13bcb0dcc21949ff000661961e9bcc98f1621216
ciemek <sharaquss@gmail.com>
2017-11-04 14:39:32 +0100

    fixed testing dependencies

--- app/src/test/java/com/android/szparag/batterygraph/ExampleUnitTest.kt
+++ app/src/test/java/com/android/szparag/batterygraph/ExampleUnitTest.kt
@@ -1,8 +1,8 @@
 package com.android.szparag.batterygraph
 
-import org.junit.Test
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
