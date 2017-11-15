e5aeb4ef4adbd4f6837d8e1c895462dc510c2079
Przemyslaw Jablonski <sharaquss@gmail.com>
2017-11-15 21:38:14 +0100

    Front screen prototype

---
+++ app/src/main/java/com/android/szparag/batterygraph/screen_front/BatteryGraphFrontActivity.kt
@@ -0,0 +1,13 @@
+package com.android.szparag.batterygraph.screen_front
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.android.szparag.batterygraph.R
+
+class BatteryGraphFrontActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_front)
+  }
+}
