40460615d0803a150346ba16bf84d9300ae0337a
sharaquss <sharaquss@gmail.com>
2017-11-20 14:47:07 +0100

    POC

--- app/src/main/java/com/android/szparag/batterygraph/widgets/DrawableAnimatedDropletWidget.kt
+++ app/src/main/java/com/android/szparag/batterygraph/widgets/DrawableAnimatedDropletWidget.kt
@@ -21,7 +21,6 @@ import android.view.animation.Animation
 import android.view.animation.AnimationSet
 import android.view.animation.AnticipateOvershootInterpolator
 import android.view.animation.Interpolator
-import android.view.animation.OvershootInterpolator
 import android.view.animation.PathInterpolator
 import android.view.animation.ScaleAnimation
 import android.widget.FrameLayout
@@ -33,6 +32,7 @@ import com.android.szparag.batterygraph.shared.utils.hide
 import com.android.szparag.batterygraph.shared.utils.inverseLerp
 import com.android.szparag.batterygraph.shared.utils.lerp
 import com.android.szparag.batterygraph.shared.utils.lerpLong
+import com.android.szparag.batterygraph.shared.utils.randomVariation
 import com.android.szparag.batterygraph.shared.utils.setListenerBy
 import com.android.szparag.batterygraph.shared.utils.show
 import com.android.szparag.batterygraph.widgets.interpolators.CutoffInterpolator
@@ -65,12 +65,13 @@ open class DrawableAnimatedDropletWidget : FrameLayout, DrawableAnimatedWidget {
 
   //todo: unify - there are vars, lateinit vars and vals here
   private var drawableView: View
-  private lateinit var circularDropletBackgroundView: View
-  private val circularBackgroundViewLayers = arrayListOf<View>()
+  private lateinit var circularDropletBackgroundView1: View
+  private lateinit var circularDropletBackgroundView2: View
+  //  private val circularBackgroundViewLayers = arrayListOf<View>()
   private val circularDropletViewLayers = arrayListOf<View>()
 
   private val random by lazy { Random() }
-  private val circularDropletBackgroundPath by lazy { generateDropletBackgroundPath() }
+//  private val circularDropletBackgroundPath by lazy { generateDropletBackgroundPath() }
 
   @DrawableRes private var drawable = android.R.color.transparent
 
@@ -113,10 +114,29 @@ open class DrawableAnimatedDropletWidget : FrameLayout, DrawableAnimatedWidget {
     Timber.d("onLayoutFirstMeasurementApplied")
 
     createCircularDropletsLayers(6)
-    circularDropletBackgroundView = createCircularBackgroundView(R.color.colorAccent1)
-    circularDropletBackgroundView.hide()
-    addView(circularDropletBackgroundView, 0)
-    animateCircularBackground(circularDropletBackgroundView)
+
+    circularDropletBackgroundView1 = createCircularBackgroundView(R.color.colorAccent1)
+    circularDropletBackgroundView1.hide()
+    addView(circularDropletBackgroundView1)
+    animateCircularBackground(
+        targetView = circularDropletBackgroundView1,
+        startTime = 0L,
+        duration = BASE_ANIMATION_BACKGROUND_LENGTH_MILLIS,
+        repeatDelay = BASE_ANIMATION_BACKGROUND_REPEAT_DELAY_MILLIS,
+        pathRandomFactor = 0.002f
+    )
+
+
+    circularDropletBackgroundView2 = createCircularBackgroundView(R.color.colorAccent1)
+    circularDropletBackgroundView2.hide()
+    addView(circularDropletBackgroundView2)
+    animateCircularBackground(
+        targetView = circularDropletBackgroundView2,
+        startTime = BASE_ANIMATION_BACKGROUND_LENGTH_MILLIS / 2,
+        duration = BASE_ANIMATION_BACKGROUND_LENGTH_MILLIS,
+        repeatDelay = BASE_ANIMATION_BACKGROUND_REPEAT_DELAY_MILLIS,
+        pathRandomFactor = 0.01f
+    )
   }
 
 
@@ -154,27 +174,28 @@ open class DrawableAnimatedDropletWidget : FrameLayout, DrawableAnimatedWidget {
   //todo:
 
   @RequiresApi(VERSION_CODES.LOLLIPOP) //todo: remove, make function createInterpolator
-  private fun animateCircularBackground(targetView: View) {
+  private fun animateCircularBackground(targetView: View, startTime: Millis, duration: Millis, repeatDelay: Millis,
+      pathRandomFactor: Float) {
     Timber.d("animateCircularBackground, targetView: ${targetView.asString()}")
     targetView.show()
     AnimationSet(false)
         .also { set ->
           set.addAnimation(createScalingAnimation(
               parentContainer = this,
-              duration = BASE_ANIMATION_BACKGROUND_LENGTH_MILLIS,
-              repeatDelay = BASE_ANIMATION_BACKGROUND_REPEAT_DELAY_MILLIS,
-              startTime = 0L,
+              duration = duration,
+              startTime = startTime,
+              repeatDelay = repeatDelay,
               xyStart = 0f,
               xyEnd = 1f,
-              interpolator = PathInterpolator(circularDropletBackgroundPath),
+              interpolator = PathInterpolator(generateDropletBackgroundPath(random, pathRandomFactor)),
               timeCutoff = 1.0f
           ))
           set.addAnimation(createFadeoutAnimation(
               parentContainer = this,
-              duration = BASE_ANIMATION_BACKGROUND_LENGTH_MILLIS,
-              repeatDelay = BASE_ANIMATION_BACKGROUND_REPEAT_DELAY_MILLIS,
-              startTime = 0L,
-              alphaStart = 0.11f,
+              duration = duration,
+              repeatDelay = repeatDelay,
+              startTime = startTime,
+              alphaStart = 0.12f,
               alphaEnd = 0.00f,
               interpolator = FastOutLinearInInterpolator(),
               timeCutoff = 0.99f
@@ -195,18 +216,14 @@ open class DrawableAnimatedDropletWidget : FrameLayout, DrawableAnimatedWidget {
         .also { set ->
           set.fillAfter = true
           set.isFillEnabled = true
-          val interpol =  if (inverseLerp < 0.67f)
-            OvershootInterpolator(lerp(1.15f, 0.95f, inverseLerp))
-          else
-            AccelerateInterpolator()
           set.addAnimation(createScalingAnimation(
               parentContainer = this,
               duration = lerpLong(first = BASE_ANIMATION_LENGTH_MIN_MILLIS, second = BASE_ANIMATION_LENGTH_MILLIS, factor = inverseLerp),
-              repeatDelay = BASE_ANIMATION_REPEAT_DELAY_MILLIS + repeatDelayAddition,
               startTime = startTime,
+              repeatDelay = BASE_ANIMATION_REPEAT_DELAY_MILLIS + repeatDelayAddition,
               xyStart = 0.00f,
-              xyEnd = lerp(0.67f, 0.93f, inverseLerp),
-              interpolator =AnticipateOvershootInterpolator(lerp(1.15f, 0.00f, inverseLerp)),
+              xyEnd = lerp(0.70f, 1.00f, inverseLerp),
+              interpolator = AnticipateOvershootInterpolator(lerp(1.33f, 0.25f, inverseLerp)),
               timeCutoff = lerp(0.8f, 0.98f, inverseLerp)
           ))
           set.addAnimation(createFadeoutAnimation(
@@ -218,7 +235,7 @@ open class DrawableAnimatedDropletWidget : FrameLayout, DrawableAnimatedWidget {
               ).toLong(),
               repeatDelay = BASE_ANIMATION_REPEAT_DELAY_MILLIS + repeatDelayAddition,
               startTime = startTime,
-              alphaStart = lerp(0.13f, 0.30f, inverseLerp),
+              alphaStart = lerp(0.15f, 0.40f, inverseLerp),
               alphaEnd = 0.00f,
               interpolator = AccelerateInterpolator(lerp(1.05f, 0.85f, inverseLerp)),
               timeCutoff = lerp(0.92f, 0.99f, inverseLerp)
@@ -229,11 +246,11 @@ open class DrawableAnimatedDropletWidget : FrameLayout, DrawableAnimatedWidget {
 
 
   @SuppressLint("BinaryOperationInTimber")
-  private fun createScalingAnimation(parentContainer: View, duration: Millis, repeatDelay: Millis, startTime: Millis,
+  private fun createScalingAnimation(parentContainer: View, duration: Millis, startTime: Millis, repeatDelay: Millis,
       xyStart: Float, xyEnd: Float, interpolator: Interpolator, timeCutoff: Float = 1.0f)
       = ScaleAnimation(xyStart, xyEnd, xyStart, xyEnd, parentContainer.width / 2f, parentContainer.height / 2f)
       .also { animation ->
-        Timber.d("createScalingAnimation, duration: $duration, repeatDelay: $repeatDelay, startTime: $startTime, " +
+        Timber.d("createScalingAnimation, duration: $duration, startTime: $startTime, repeatDelay: $repeatDelay, " +
             "xyStart: $xyStart, xyEnd: $xyEnd, interpolator: $interpolator, timeCutoff: $timeCutoff")
         animation.duration = duration
         animation.startOffset = startTime
@@ -243,8 +260,8 @@ open class DrawableAnimatedDropletWidget : FrameLayout, DrawableAnimatedWidget {
         animation.setListenerBy(onRepeat = { animation.startOffset = repeatDelay })
       }
 
-  private fun createFadeoutAnimation(parentContainer: View, duration: Millis, startTime: Millis, repeatDelay: Millis, alphaStart: Float,
-      alphaEnd: Float, interpolator: Interpolator, timeCutoff: Float) =
+  private fun createFadeoutAnimation(parentContainer: View, duration: Millis, startTime: Millis, repeatDelay: Millis,
+      alphaStart: Float, alphaEnd: Float, interpolator: Interpolator, timeCutoff: Float) =
       AlphaAnimation(alphaStart, alphaEnd)
           .also { animation ->
             Timber.d("createFadeoutAnimation, duration: $duration, startTime: $startTime, repeatDelay: $repeatDelay, " +
@@ -311,14 +328,14 @@ open class DrawableAnimatedDropletWidget : FrameLayout, DrawableAnimatedWidget {
   }
 
   //proven that this feels good in the ui with serious laboratory testing. true story
-  private fun generateDropletBackgroundPath() = Path().apply {
+  private fun generateDropletBackgroundPath(random: Random, randomFactor: Float = 0.005f) = Path().apply {
     moveTo(0.000f, 0.000f)
-    quadTo(0.065f, 0.325f, 0.150f, 0.400f)
-    lineTo(0.330f, 0.300f)
-    quadTo(0.390f, 0.630f, 0.420f, 0.690f)
-    lineTo(0.690f, 0.480f)
-    quadTo(0.725f, 0.85f, 0.740f, 0.900f)
-    lineTo(0.930f, 0.710f)
+    quadTo(0.065f, 0.325f, 0.150f, 0.400f.randomVariation(random, randomFactor))
+    lineTo(0.330f, 0.300f.randomVariation(random, randomFactor / 2f))
+    quadTo(0.390f, 0.630f, 0.420f, 0.690f.randomVariation(random, randomFactor))
+    lineTo(0.690f, 0.480f.randomVariation(random, randomFactor / 2f))
+    quadTo(0.725f, 0.85f, 0.740f, 0.900f.randomVariation(random, randomFactor))
+    lineTo(0.930f, 0.710f.randomVariation(random, randomFactor / 2f))
     quadTo(0.965f, 0.925f, 1.000f, 1.000f)
   }
 
